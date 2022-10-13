6469c319523de59f88c748be587be284fa4fb437
auberr <auberr@daum.net>
2022-10-13 21:39:00 +0900

    Add login feature

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
+from django.contrib.auth import authenticate, login as loginsession
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def signup(request):
@@ -22,3 +25,33 @@ def signup(request):
         return HttpResponse("허용되지 않은 메소드")
 
 
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(username, password)
+        if user is not None:
+            loginsession(request, user)
+            return redirect('users:user')
+        
+        else:
+            return HttpResponse("로그인 실패")
+
+def user(request):
+    print(request.user)
+    print(request.user.date_joined)
+    return HttpResponse(request.user)
+
+def profile(request, username):
+    print(username)
+    user = User.objects.get(username=username)
+    if user is None:
+        user = get_object_or_404(User, username=username)
+    elif user is not None :
+        context = {
+            "user": user
+        }
+    return render(request, 'profile.html', context)
